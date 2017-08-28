7752a4c103a11812f4b98b06f8fd195195a95cf8
Jason L Perry <jasper@ambethia.com>
2017-08-28 10:51:38 -0400

    Prop types

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class App extends Component {
   state = {
@@ -18,13 +19,15 @@ class App extends Component {
           { id: 3, name: 'Porpoise', image: 'http://elelur.com/data_images/mammals/porpoise/porpoise-02.jpg' }
         ]
       })
-    }, 2500)
+    }, 500)
   }
 
   animals() {
     if (this.state.animals.length === 0) {
       return <h2>No Animals Found. Sad.</h2>
     } else {
+      // An alternative to:
+      //   return animals.map(animal => <li>...</li>) is to extract to it's own component.
       return this.state.animals.map(animal => <Animal {...animal} key={animal.id} />)
     }
   }
@@ -53,5 +56,10 @@ const Animal = ({ image, name }) =>
       </figcaption>
     </figure>
   </li>
+
+Animal.propTypes = {
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired
+}
 
 export default App
